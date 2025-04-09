77bdbcf5c649fbe7340cc283c93519ba69fd9f4d
daffarizr <daffarizr@gmail.com>
2025-04-09 12:25:23 +0700

    Update script.js
    Update bagian search

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// untuk memunculkan navbar ketika klik tombol menu
+// toggle clast active untuk memunculkan navbar ketika klik tombol menu
 const navbarNav = document.querySelector('.navbar-nav');
 
 document.querySelector('#hamburger-menu').
@@ -6,13 +6,29 @@ onclick = () => {
   navbarNav.classList.toggle('active');
 };
 
+// toggle clast active untuk memunculkan navbar ketika klik tombol searh
+
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+document.querySelector('#search-button').onclick = (e) => {
+  searchForm.classList.toggle('active');
+  searchBox.focus();
+  e.preventDefault();
+}
+
 
 // ketika pencet selain tombol menu bisa keluar
 
-const hamburger = document.querySelector('#hamburger-menu');
+const hm = document.querySelector('#hamburger-menu');
+const sb = document.querySelector('#search-button');
 
 document.addEventListener('click', function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove('active');
+  }
+  
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove('active');
   }
 });
